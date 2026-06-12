cbc6a5a0c752b70ad94d9e757358f8a69982b987
Stefan le Roux <stefan@vivtec.co.za>
2026-06-12 16:57:41 +0200

    Added /base

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://vivtec-net.github.io/drwaldo.co.za",
+	site: "https://vivtec-net.github.io",
+	base: "/drwaldo.co.za",
 	markdown: {
 		processor: satteri({
 			features: { directive: true },
